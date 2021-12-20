480b93d7e50c49219589f4fb26f1a2d1aa1d0ef8
frida stenberg <fridastenberg97@hotmail.com>
2021-12-20 16:47:24 +0100

    sign in

--- the_book_catalogue/src/pages/SignIn.js
+++ the_book_catalogue/src/pages/SignIn.js
@@ -3,7 +3,12 @@ import Styles from "../styles/SignIn.module.css"
 const SignIn = () => {
     return(
         <div className={Styles.signInContainer}>
-            <h1>hej</h1>
+            <form className={Styles.formWrapper}>
+                <h1 className={Styles.formHeader} >Sign In</h1>
+                <input className={Styles.input} type="email" placeholder="Email" />
+                <input className={Styles.input} type="password" placeholder="Password"/>
+                <button className={Styles.signInBtn}>Sign In</button>
+            </form>
         </div>
     )
 }
