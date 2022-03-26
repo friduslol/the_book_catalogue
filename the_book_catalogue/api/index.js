368b0d5ee667ba43528e8d1b658a9738a5af3f55
frida stenberg <fridastenberg97@hotmail.com>
2022-03-26 17:13:37 +0100

    setup mongodb

--- the_book_catalogue/api/index.js
+++ the_book_catalogue/api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const port = 3001;
 
@@ -6,6 +7,15 @@ const port = 3001;
 const app = express();
 
 app.use(express.json());
+
+//mongoose connection
+const mongoDB = "mongodb+srv://dbTest:dbTestPass@cluster0.nuenf.mongodb.net/the_book_catalogue?retryWrites=true&w=majority"
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//error handeling
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 //start Server
 app.listen(port, (err) => {
