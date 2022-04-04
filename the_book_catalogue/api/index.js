5830851eba85e3e06dbc24761753a0426fb3db82
frida stenberg <fridastenberg97@hotmail.com>
2022-04-04 22:04:07 +0200

    setup for book route

--- the_book_catalogue/api/index.js
+++ the_book_catalogue/api/index.js
@@ -3,19 +3,30 @@ const mongoose = require("mongoose");
 
 const port = 3001;
 
+//mongoose connection
+const mongoDB = "mongodb+srv://dbTest:dbTestPass@cluster0.nuenf.mongodb.net/the_book_catalogue?retryWrites=true&w=majority"
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//routes
+const bookRoutes = require("./routes/bookRoutes");
+
+
 //server setup
 const app = express();
 
 app.use(express.json());
 
-//mongoose connection
-const mongoDB = "mongodb+srv://dbTest:dbTestPass@cluster0.nuenf.mongodb.net/the_book_catalogue?retryWrites=true&w=majority"
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //error handeling
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+
+//routes setup
+app.use("api/v1/book", bookRoutes);
 
 //start Server
 app.listen(port, (err) => {
