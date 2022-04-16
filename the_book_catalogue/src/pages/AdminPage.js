61392c0dc6c4f3cc41d34e12861a6dfc40ba27a3
frida stenberg <fridastenberg97@hotmail.com>
2022-04-16 14:38:46 +0200

    working on add book form

--- the_book_catalogue/src/pages/AdminPage.js
+++ the_book_catalogue/src/pages/AdminPage.js
@@ -1,9 +1,9 @@
 import Styles from "../styles/Admin.module.css"
 import { useState, useEffect, useRef } from "react";
-import { use } from "../../api/routes/bookRoutes";
+
 
 const AdminPage = () => {
-    const [input, setInput] = useState('')
+    const [coverImg, setCoverImg] = useState(null)
     const titleRef = useRef()
     const descRef = useRef()
     const authorRef = useRef()
@@ -11,61 +11,66 @@ const AdminPage = () => {
     const pubYearRef = useRef()
     const pagesRef = useRef()
     const categoryRef = useRef()
-    const coverImgRef = useRef()
-
 
     const uploadImg = () => {
-       let ref = document.getElementById('fileUpload')
-       ref.click()
-       console.log(ref)
+        let ref = document.getElementById('fileUpload')
+        ref.click()
     }
 
-   useEffect(() => {
-       if(input.length) {
-            let imageName = input.slice(12)
-            if(imageName.length < 16) {
-                document.getElementById('fileName').innerHTML = imageName
-            } else {
-                document.getElementById('fileName').innerHTML = imageName.slice(0, 13) + "..."
-            }
-       } else {
-           return;
-       }
-   }, [input])
+   const handleFileChange = (e) => {
+        if (e.target.files[0]) {
+            setCoverImg(e.target.files[0])
+        }
 
-   const handleAddBook = async () => {
+    }
+   const handleAddBook = async (e) => {
+
+    e.preventDefault()
 
         let bookObj = {
-            title: titleRef,
-            description: descRef,
-            author: authorRef,
-            isbn: isbnRef,
-            publicationYear: pubYearRef,
-            pages: pagesRef,
-            category: categoryRef,
+            title: titleRef.current.value,
+            description: descRef.current.value,
+            author: authorRef.current.value,
+            isbn: isbnRef.current.value,
+            publicationYear: pubYearRef.current.value,
+            pages: pagesRef.current.value,
+            category: categoryRef.current.value,
             rating: 0,
-            coverImg: coverImgRef,
+            coverImg: coverImg ? coverImg : "No cover available",
         }
+
+        console.log("bookObj:", bookObj)
    }
 
     return(
         <div className={Styles.adminContainer}>
             <div className={Styles.formsWrapper}>
-                <form className={Styles.formWrapper}>
-                    <h1 className={Styles.formHeader} >Add a book</h1>
-                    <input className={Styles.input} type="number" placeholder="idNumber" />
-                    <input className={Styles.input} type="Name" placeholder="Title"/>
+                <form className={Styles.formWrapper} onSubmit={handleAddBook}>
+                    <h1 className={Styles.formHeader}>Add a book</h1>
+                    <input className={Styles.input} type="text" placeholder="Title" ref={titleRef} required/>
+                    <input className={Styles.input} type="text" placeholder="Author" ref={authorRef} required/>
+                    <input className={Styles.input} type="number" placeholder="Publication year" ref={pubYearRef} required/>
+                    <input className={Styles.input} type="text" placeholder="Description" ref={descRef} required/>
+                    <input className={Styles.input} type="text" placeholder="Category" ref={categoryRef} required/>
+                    <input className={Styles.input} type="number" placeholder="Pages" ref={pagesRef} required/>
+                    <input className={Styles.input} type="text" placeholder="Isbn" ref={isbnRef} required/>
 
                     <div className={Styles.uploadImgWrapper}>
-                        <input type="button" className={Styles.uploadButton} value="Choose image" onClick={() => uploadImg()} />
-                        <input type="file" name="upload" accept="image/*" id="fileUpload" value={input} onInput={e => setInput(e.target.value)} className={Styles.fileUpload} />
+                        <input type="button" className={Styles.uploadButton} value="Choose image"  onClick={() => uploadImg()}/>
+                        <input type="file" name="upload" accept="image/*" id="fileUpload" className={Styles.fileUpload}  onChange={handleFileChange}/>
                         <div className={Styles.filenameWrapper}>
-                            <span id="fileName" className={Styles.fileName}>...</span>
+                            <span id="fileName" className={Styles.fileName}>
+                                {
+                                    coverImg
+                                        ? `${coverImg.name}`
+                                        : "..."
+                                }
+                            </span>
                         </div>
 
                     </div>
 
-                    <button onClick={handleAddBook} className={Styles.actionBtn}>Add</button>
+                    <button type="submit" className={Styles.actionBtn}>Add</button>
                 </form>
 
                 <form className={Styles.formWrapper}>
