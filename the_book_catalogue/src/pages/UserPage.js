57d20a4c16555f9316cf613fdcd1a208aad604f9
frida stenberg <fridastenberg97@hotmail.com>
2022-05-12 15:48:37 +0200

    cursor

--- the_book_catalogue/src/pages/UserPage.js
+++ the_book_catalogue/src/pages/UserPage.js
@@ -43,7 +43,6 @@ const UserPage = () => {
 
                     <button className={Styles.logOutBtn} onClick={handleLogout}>Logout</button>
                 </div>
-                <button className={Styles.deleteAccBtn}>Delete Account</button>
             </div>
 
             <div className={Styles.bookListsContainer}>
